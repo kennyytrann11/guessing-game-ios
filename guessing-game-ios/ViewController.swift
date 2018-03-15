be8dcf11bae2581cd0739c0bd5d6b8de460ce9ed
Kenny Tran <kennytran@Interapts-MacBook-Pro-174.local>
2018-03-15 16:03:20 -0400

    guessing game basics

--- guessing-game-ios/ViewController.swift
+++ guessing-game-ios/ViewController.swift
@@ -13,9 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var userGuessField: UITextField!
     @IBOutlet weak var userFeedbackLabel: UILabel!
     @IBOutlet weak var rulesLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
     
     var randomNumber = Int(arc4random_uniform(100))
     var numberOfGuesses = 5
+    
     var rulesLabelText = """
     *****************RULES*****************
     Rule 1. Guess a number between 0 and 100
@@ -23,13 +25,16 @@ class ViewController: UIViewController {
     Rule 3. You will be given hints
 
 """
+    var buttonText = "Guess!"
+    var zeroGuesses = "Try again!"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
         rulesLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         rulesLabel.text = rulesLabelText
-    }
+        button.setTitle(buttonText, for: .normal)
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -37,20 +42,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func guessButtonPressed(_ sender: Any) {
-        let guesses = Int(userGuessField!)!
-        if guesses > randomNumber {
-            numberOfGuesses -= 1
-            userFeedbackLabel.text = "Your guess was too low! You have \(numberOfGuesses) left."
-        } else if guesses < randomNumber {
-            numberOfGuesses -= 1
-            userFeedbackLabel.text = "Your guess was too high! You have \(numberOfGuesses) left."
-        } else {
-            userFeedbackLabel.text = "You guessed correcly!"
+        if button.title(for: .normal) == zeroGuesses {
+            button.setTitle(buttonText, for: .normal)
         }
-            if numberOfGuesses == 0 {
-                userFeedbackLabel.text = "You have run out of guesses!"
+        if numberOfGuesses != 1 {
+            numberOfGuesses -= 1
+            let guess = Int(userGuessField.text!)!
+            if guess > randomNumber {
+                userFeedbackLabel.text = "Your guess was too high! You have \(numberOfGuesses) guesses left."
+            } else if guess < randomNumber {
+                userFeedbackLabel.text = "Your guess was too low! You have \(numberOfGuesses) guesses left."
+            } else {
+                userFeedbackLabel.text = "You guessed correcly!"
+                button.setTitle(zeroGuesses, for: .normal)
             }
+        } else {
+            var noMoreGuesses = String(describing: button)
+            userFeedbackLabel.text = "You have run out of guesses! The correct answer is \(randomNumber)"
+            button.setTitle(zeroGuesses, for: .normal)
+            randomNumber = Int(arc4random_uniform(100))
+            numberOfGuesses = 5
+            
+            }
+        }
     }
-}
 
 
